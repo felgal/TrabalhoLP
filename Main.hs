1e97b5c5791428d3204f47e77da8f8c9884fa12d
felgal <rag.felipe@gmail.com>
2018-11-07 21:04:43 -0200

    Primeira versão do "<>" - Com bugs
    Bug de head, desconhecido o motivo, aparentemente não crasha se é para dar a resposta certa, mas crasha se é para errar.

--- Main.hs
+++ Main.hs
@@ -30,27 +30,15 @@ elemisin x y =
   else
     elemisin (tail x) y
 
---diamond :: Vertex -> Graph -> [Vertex] -> Bool
---diamond vertice grafo verdades =
---  let execucao = reachable' grafo vertice
---  in
---    if elemisin execucao verdades
---      then
---        True
---    else
---      False
-
-diamond :: Vertex -> Graph -> [Vertex]
-diamond vertice grafo = reachable' grafo vertice
-
-
-
-(?) :: (Ord a) => a -> [a] -> Bool
-(?) _ [] = False
-(?) x y
-  | x == head y = True
-  | otherwise = (?) x (tail y)
-
+diamond :: Vertex -> Graph -> [Vertex] -> Bool
+diamond vertice grafo verdades =
+  let execucao = reachable' grafo vertice
+  in
+    if elemisin execucao verdades
+      then
+        True
+    else
+      False
 
 reachable' :: Graph -> Vertex -> [Vertex]
 reachable' grafo vertice =
@@ -62,6 +50,12 @@ reachable' grafo vertice =
       vizinhos
     else
       vizinhos'
+
+(?) :: (Ord a) => a -> [a] -> Bool
+(?) _ [] = False
+(?) x y
+  | x == head y = True
+  | otherwise = (?) x (tail y)
 
 vertices' :: Graph -> (Vertex, Vertex)
 vertices' grafo = (minimum $ vertices grafo, maximum $ vertices grafo)
@@ -124,29 +118,119 @@ substituiPDL pdl quantAtual quantTotal =
           '/'
     in if c=='/'
       then []
-      else if (not (c=='>')) && (not (c=='-')) && (not (c=='&')) && (not (c=='|')) && (not (c=='(')) && (not (c==')')) && (not (c=='0')) && (not (c=='1')) && (not (c=='2')) && (not (c=='3')) && (not (c=='4')) && (not (c=='5')) && (not (c=='6')) && (not (c=='7')) && (not (c=='8')) && (not (c=='9'))
+      else if (not (c=='<')) && (not (c=='>')) && (not (c=='-')) && (not (c=='&')) && (not (c=='|')) && (not (c=='(')) && (not (c==')')) && (not (c=='0')) && (not (c=='1')) && (not (c=='2')) && (not (c=='3')) && (not (c=='4')) && (not (c=='5')) && (not (c=='6')) && (not (c=='7')) && (not (c=='8')) && (not (c=='9'))
         then ((substituiPDL (substituiCharInPDL pdl c (quantTotal-quantAtual)) (quantAtual-1) quantTotal))
         else ([c]++(substituiPDL (tail pdl) quantAtual quantTotal))
 
-verificaPDLcomEntrada :: String -> Bool
-verificaPDLcomEntrada pdl =
+verificaEstado :: Int -> [Int] -> Bool
+verificaEstado estado aceitos=
+  let
+    estAtual = head(aceitos)
+  in if estAtual==estado
+    then True
+    else False || verificaEstado estado (tail aceitos)
+
+fazPrograma :: [String] ->Graph ->[Int] ->[Int] -> Bool
+fazPrograma comandos grafo estados estadosAceitos=
+  let
+    estadoAtual = let
+      in if (length estados)>0
+        then
+          (head estados)
+        else
+          (-1)
+    comando = let
+      in if (length comandos)>0
+        then
+          (head comandos)
+        else
+          "0"
+  in if comando=="0"
+    then verificaEstado estadoAtual estadosAceitos
+    else if estadoAtual==(-1)
+      then False
+      else if comando=="<"
+        then (fazPrograma (tail comandos) grafo (reachable grafo estadoAtual) estadosAceitos)  || (fazPrograma comandos grafo (tail estados) estadosAceitos)
+        else False
+
+dividePrograma :: String -> [String]
+dividePrograma pdl=
+  let
+    prim = let
+      in if (length pdl)>0
+        then
+          (head pdl)
+        else
+          '/'
+    terc = let
+      in if (length pdl)>2
+        then
+          head (tail( tail pdl))
+        else
+          '/'
+    ult = let
+      in if (length pdl)>1
+        then
+          last (init pdl)
+        else
+          '/'
+  in if ult=='/' || prim=='/' || terc=='/'
+    then []
+    else if ult=='?'
+      then
+        (dividePrograma (init( init( init (init pdl))))) ++ ["?"]
+      else if terc=='*'
+        then ["*",[prim]] ++ (dividePrograma (tail( tail( tail( tail( tail pdl))))))
+        else if prim=='['
+          then ["["] ++ (dividePrograma (tail( tail (tail ( tail pdl)))))
+          else if prim=='<'
+            then ["<"] ++ (dividePrograma (tail( tail (tail ( tail pdl)))))
+            else ["<"] ++ (dividePrograma (tail (tail (tail ( tail pdl)))))
+
+achaVar :: String -> Int
+achaVar pdl =
+  let
+    cAtual = let
+      in if (length pdl)>0
+        then
+          head pdl
+        else
+          '/'
+  in if  (cAtual =='0') || (cAtual =='1') ||  (cAtual =='2') ||  (cAtual =='3') ||  cAtual =='4' ||  cAtual =='5' ||  cAtual =='6' ||  cAtual =='7' ||  cAtual =='8'
+    then (digitToInt cAtual)
+    else if cAtual=='/'
+      then 3
+      else achaVar (tail pdl)
+
+retornaPosVal :: [[Int]] -> Int -> [Int]
+retornaPosVal vals pos=
+  let
+    val = head vals
+  in if pos==0
+    then val
+    else retornaPosVal vals (pos-1)
+
+
+
+verificaPDLcomEntrada :: Graph -> String ->[[Int]] -> Bool
+verificaPDLcomEntrada grafo pdl posVals =
   let
     posDiv = achaDivisao pdl 0
     in if posDiv==0
       then
-        True
+        fazPrograma (dividePrograma pdl) grafo (vertices grafo) (retornaPosVal posVals (achaVar pdl))
       else let
         conector = pdl !! posDiv
         parte1 = take (posDiv-2) (drop 1 pdl)
         parte2 = drop (posDiv+2) (take 1 pdl)
         in if conector=='&'
-          then ((verificaPDLcomEntrada parte1) && (verificaPDLcomEntrada parte2))
+          then ((verificaPDLcomEntrada grafo parte1 posVals) && (verificaPDLcomEntrada grafo parte2 posVals))
           else if conector=='|'
-            then ((verificaPDLcomEntrada parte1) || (verificaPDLcomEntrada parte2))
-            else if conector=='>'
-              then ((not(verificaPDLcomEntrada parte1)) || (verificaPDLcomEntrada parte2))
+            then ((verificaPDLcomEntrada grafo parte1 posVals) || (verificaPDLcomEntrada grafo parte2 posVals))
+            else if conector=='%'
+              then ((not(verificaPDLcomEntrada grafo parte1 posVals)) || (verificaPDLcomEntrada grafo parte2 posVals))
               else if conector=='-'
-                then ((verificaPDLcomEntrada parte1) && (verificaPDLcomEntrada parte2) || (not(verificaPDLcomEntrada parte1)) && (not(verificaPDLcomEntrada parte2)) )
+                then ((verificaPDLcomEntrada grafo parte1 posVals) && (verificaPDLcomEntrada grafo parte2 posVals) || (not(verificaPDLcomEntrada grafo parte1 posVals)) && (not(verificaPDLcomEntrada grafo parte2 posVals)) )
                 else False
 
 
@@ -198,10 +282,15 @@ main = do
   varsSemi <- return $ geraConcatDeInts tuplasVars
   print varsSemi
 
-  --Entra com o pdl no formato ((q)&(p))-(p)
+
+
   entPdl <- getLine
-  print $ verificaPDLcomEntrada entPdl
 
   --realiza a substituição de caracteres
   novoPdl <- return $ substituiPDL entPdl (length tuplasVars) (length tuplasVars)
   print $ novoPdl
+
+  print $ take (15-2) (drop 1 novoPdl)
+
+  --Entra com o pdl no formato ((q)&(p))-(p)
+  print $ verificaPDLcomEntrada graph entPdl varsSemi
