3a54bb232d911bd92339fbd0d7dbbf4bc9588e7b
felgal <rag.felipe@gmail.com>
2018-11-07 17:37:45 -0200

    Entrada de verdadeiros de variáveis de PDL

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import           Control.Monad
 import           Data.Bool
+import           Data.Char
 import           Data.Foldable
 import           Data.Graph
 import           Data.List.Split
@@ -46,6 +47,38 @@ achaDivisao pdl quantEnt =
           else
             ((achaDivisao (tail pdl) (quantEnt) ) + 1)
 
+substituiCharInPDL :: String->Char->Int->String
+substituiCharInPDL pdl cTroca valTroca =
+  let
+    valTrocaChar = show valTroca
+    valTrocaCharF = intToDigit valTroca
+    c = let
+      in if (length pdl)>0
+        then
+          (head pdl)
+        else
+          '/'
+    in if c=='/'
+      then []
+      else if c==cTroca
+        then ([valTrocaCharF]++ (substituiCharInPDL (tail pdl) cTroca valTroca))
+        else ([c]++ (substituiCharInPDL (tail pdl) cTroca valTroca))
+
+substituiPDL :: String->Int->Int->String
+substituiPDL pdl quantAtual quantTotal =
+  let
+    c = let
+      in if (length pdl)>0
+        then
+          (head pdl)
+        else
+          '/'
+    in if c=='/'
+      then []
+      else if (not (c=='>')) && (not (c=='-')) && (not (c=='&')) && (not (c=='|')) && (not (c=='(')) && (not (c==')')) && (not (c=='0')) && (not (c=='1')) && (not (c=='2')) && (not (c=='3')) && (not (c=='4')) && (not (c=='5')) && (not (c=='6')) && (not (c=='7')) && (not (c=='8')) && (not (c=='9'))
+        then ((substituiPDL (substituiCharInPDL pdl c (quantTotal-quantAtual)) (quantAtual-1) quantTotal))
+        else ([c]++(substituiPDL (tail pdl) quantAtual quantTotal))
+
 verificaPDLcomEntrada :: String -> Bool
 verificaPDLcomEntrada pdl =
   let
@@ -67,10 +100,27 @@ verificaPDLcomEntrada pdl =
                 then ((verificaPDLcomEntrada parte1) && (verificaPDLcomEntrada parte2) || (not(verificaPDLcomEntrada parte1)) && (not(verificaPDLcomEntrada parte2)) )
                 else False
 
+
+geraConcatDeInts :: [String] ->[[Int]]
+geraConcatDeInts str =
+  let
+    c = let
+      in if (length str)>0
+        then
+          (head str)
+        else
+          "/"
+  in if c=="/"
+    then
+      []
+    else
+      [(map read (splitOn "," c) :: [Int])] ++ (geraConcatDeInts (tail str))
+
 main = do
   --Entrada da quantidade de nós
-  entNos<- getLine
-  let quantNos = (read entNos :: Int)
+  --entNos<- getLine
+  --let quantNos = (read entNos :: Int)
+
   --Entrada das areas no formato 1/2,3;2/1,3;3/1
   entArestas<- getLine
   tuplasNos <- return $ splitOn ";" entArestas
@@ -89,6 +139,20 @@ main = do
   let (graph, vertexMap) = graphFromEdges' nos
   print $ edges graph
 
+  --entra com a quantidade de variaveis
+  --entVar<- getLine
+  --let quantVar = (read entVar :: Int)
+
+  --cria a lista de valores verdadeiros a partir da entrada,exemplo 0,1;0,1,2
+  entVars<- getLine
+  tuplasVars <- return $ splitOn ";" entVars
+  varsSemi <- return $ geraConcatDeInts tuplasVars
+  print varsSemi
+
   --Entra com o pdl no formato ((q)&(p))-(p)
   entPdl <- getLine
   print $ verificaPDLcomEntrada entPdl
+
+  --realiza a substituição de caracteres
+  novoPdl <- return $ substituiPDL entPdl (length tuplasVars) (length tuplasVars)
+  print $ novoPdl
